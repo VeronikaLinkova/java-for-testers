ccd1ca913022acd03ecf985141aeb0c0f5ae7acc
VeronikaLinkova <v.a.krilova@yandex.ru>
2021-07-06 22:45:35 +0300

    Создание классов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,18 +5,12 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         hello("world");
         Square s = new Square(5);
-        System.out.println(area(s));
+        System.out.println(s.area());
 
         Rectangle r = new Rectangle(4,6);
-        System.out.println(area(r));
+        System.out.println(r.area());
     }
     public static void hello(String somebody){
         System.out.println("Hello, "+somebody+" !!!!");
-    }
-    public static double area(Square s){
-        return s.l*s.l;
-    }
-    public static double area(Rectangle r){
-        return r.a*r.b;
     }
 }
