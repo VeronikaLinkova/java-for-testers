1ad0700686a808093bfa67d89f3a1c257d9f177f
VeronikaLinkova <v.a.krilova@yandex.ru>
2021-07-06 18:59:00 +0300

    Создание классно и объектов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,8 +4,19 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         hello("world");
+        Square s = new Square(5);
+        System.out.println(area(s));
+
+        Rectangle r = new Rectangle(4,6);
+        System.out.println(area(r));
     }
     public static void hello(String somebody){
         System.out.println("Hello, "+somebody+" !!!!");
+    }
+    public static double area(Square s){
+        return s.l*s.l;
+    }
+    public static double area(Rectangle r){
+        return r.a*r.b;
     }
 }
