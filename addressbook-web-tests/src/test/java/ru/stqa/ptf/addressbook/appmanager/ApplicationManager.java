361c416a17b0a58db96d9026bac019931992a801
VeronikaLinkova <v.a.krilova@yandex.ru>
2021-07-21 01:11:53 +0300

    Реализация использования браузера Chrome и FireFox
    1) в классе ApplicationManager в методе init добавлена конструкция if-else для выбора браузера
    2) в классе ApplicationManager доработан конструктор - передается параметр для определения браузера
    3) в классе TestBase при создании объекта ApplicationManager передаем в параметре информацию о браузере (использован BrowserType.CHROME для проверки работоспособности)
    4) chromdraiver.exe в проект не помещался, расположен в системной папке на диске C

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ApplicationManager.java
@@ -3,7 +3,11 @@ package ru.stqa.ptf.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.browser.Browser;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,9 +18,23 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        }
+        else if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        }
+        else if (browser.equals(BrowserType.IE)){
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
