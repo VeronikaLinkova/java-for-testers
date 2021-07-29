c57bd26b5e8ccffd608a85e9b9010faf861a0071
VeronikaLinkova <v.a.krilova@yandex.ru>
2021-07-30 01:00:03 +0300

    Вычисление максимального идентификатора

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/test/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/test/GroupCreationTests.java
@@ -3,8 +3,6 @@ package ru.stqa.ptf.addressbook.test;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.ptf.addressbook.model.GroupData;
-
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
@@ -28,7 +26,19 @@ public class GroupCreationTests extends TestBase {
     app.logOut();
     //Assert.assertEquals(after,before + 1);
     Assert.assertEquals(after.size(),before.size() + 1);
+    //after.
+    /*
+    int max = 0;
+    for (GroupData g: after){
+      if (g.getId()>max){
+        max = g.getId();
+      }
+    }
+     */
+
     before.add(group);
+    int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId();
+    group.setId(max);
     Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 }
