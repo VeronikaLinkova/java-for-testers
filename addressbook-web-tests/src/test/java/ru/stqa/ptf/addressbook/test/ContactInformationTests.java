964154b51f58a79b80fd6cdd5bdd0673f675a36a
VeronikaLinkova <v.a.krilova@yandex.ru>
2021-08-11 01:06:06 +0300

    Объединение нескольких классов в один класс ContactInformationTests
    Доработка по замечанию тренера:
    1) проверки  телефонов, почтового адреса и электронных адресов перенесены в один класс

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/test/ContactInformationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/test/ContactInformationTests.java
@@ -1,7 +1,5 @@
 package ru.stqa.ptf.addressbook.test;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.ContactData;
@@ -12,14 +10,15 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
-public class ContactPhoneTests extends TestBase{
+public class ContactInformationTests extends TestBase{
     @BeforeTest()
     public void ensurePreconditions(){
         if (app.contact().list().size()==0){
             app.goTo().goToAddNewContactPage();
             app.contact().create(new ContactData().withFirstname("Veronika1990").withMiddlename("Alexandrovna").
                     withLastname("Linkova").withNikcname("Nika")
-                    .withHomePhone("90201111").withMobilePhone("945-99-50-20").withWorkPhone("(90)"));
+                    .withHomePhone("90201111").withMobilePhone("945-99-50-20").withWorkPhone("(90)")
+            .withAddress("test").withEmail("email1").withEmail2("email2").withEmail3("email1@yandex.ru"));
             app.goTo().homePage();
         }
     }
@@ -35,10 +34,38 @@ public class ContactPhoneTests extends TestBase{
                 equalTo(mergePhones(contactInfoFromEditForm)));
     }
 
+    @Test()
+    public void testContactEmail() throws Exception{
+        app.goTo().homePage();
+
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+        assertThat(contact.getAllEmailAdress(),
+                equalTo(mergeEmailAddress(contactInfoFromEditForm)));
+    }
+
+    @Test()
+    public void testContactAddress() throws Exception{
+        app.goTo().homePage();
+
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+        assertThat(contact.getAddress(),
+                equalTo(contactInfoFromEditForm.getAddress()));
+    }
+
     private String mergePhones(ContactData contact) {
         return Arrays.asList(contact.getHomePhone(),contact.getMobilePhone(),
                 contact.getWorkPhone()).stream().filter((s)->!s.equals(""))
-                .map(ContactPhoneTests::cleaned)
+                .map(ContactInformationTests::cleaned)
+                .collect(Collectors.joining("\n"));
+    }
+
+    private String mergeEmailAddress(ContactData emailAddress) {
+        return Arrays.asList(emailAddress.getEmail(),emailAddress.getEmail2(),
+                emailAddress.getEmail3()).stream().filter((s)->!s.equals(""))
                 .collect(Collectors.joining("\n"));
     }
 
